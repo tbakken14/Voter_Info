db7ed540a98de20b3f6f6d803d3d007da74e0037
Thomas Bakken <tbakken14@gmail.com>
2023-06-01 09:36:11 -0700

    Add form structure and submit functionality
    Co-authored-by: Jake Elsberry <Jelsberry96@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+function OnSubmission(event) {
+    event.preventDefault();
+    console.log(age.value);
+};
+
 window.onload = function () {
     const form = document.querySelector("form");
-}
+    form.onsubmit = OnSubmission;
+    const age = document.getElementById("age");
+
+};
+
