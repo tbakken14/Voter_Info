eb583843163f27d8f548c00ec2d491e1dc984945
Thomas Bakken <tbakken14@gmail.com>
2023-06-01 10:17:23 -0700

    Add functionality for voters and minors
    Co-authored-by: Jake Elsberry <Jelsberry96@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,59 @@
+//Global variables
+let form;
+let age;
+let voterInfo;
+let minorInfo;
+let isMinor = false;
+
+//UI Functions
 function OnSubmission(event) {
     event.preventDefault();
-    console.log(age.value);
-};
+    AgeLogic(parseInt(age.value));
+}
 
-window.onload = function () {
-    const form = document.querySelector("form");
+//Parameter: age of user type Number
+//display correct info based on result
+function AgeLogic(age_value) {
+    console.log(age_value);
+    if (Number.isNaN(age_value)) {
+        alert("Please enter a valid number.");
+    }
+    else if (age.value >= 18) {
+        if (isMinor) {
+            alert("Nice try");
+        }
+        else {
+            swapClassNameHidden(voterInfo, minorInfo);
+        }
+    }
+    else {
+        isMinor = true;
+        swapClassNameHidden(minorInfo, voterInfo);
+    }
+}
+
+function swapClassNameHidden(makeHidden, makeVisible) {
+    makeVisible.setAttribute("class", "hidden");
+    makeHidden.removeAttribute("class");
+}
+
+//Initialization functions
+function InitializeVariables() {
+    form = document.querySelector("form");
+    age = document.getElementById("age");
+    voterInfo = document.getElementById("voter_info");
+    minorInfo = document.getElementById("minor_info");
+}
+
+function InitializeUIFunctions() {
     form.onsubmit = OnSubmission;
-    const age = document.getElementById("age");
+}
 
+//Initialize on load
+window.onload = function () {
+    InitializeVariables();
+    InitializeUIFunctions();
 };
+
+
 
